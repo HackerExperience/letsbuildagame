256afce078a32d5ff60c0b7e4cffa9420e55a0dd
mememori <charlotte.elixir@gmail.com>
2016-04-28 06:31:57 -0300

    Timeline

---
+++ src/js/timeline_popover.js
@@ -0,0 +1,25 @@
+(function() {
+  $("#timeline-button-1").popover({
+    container: ".timeline-graph",
+    content: "Lorem ipsum",
+    title: "foobar"
+  });
+
+  $("#timeline-button-2").popover({
+    container: ".timeline-graph",
+    content: "Lorem ipsum",
+    title: "bar"
+  })
+
+  $("#timeline-button-3").popover({
+    container: ".timeline-graph",
+    content: "Lorem ipsum",
+    title: "baz"
+  })
+
+  $("#timeline-button-4").popover({
+    container: ".timeline-graph",
+    content: "Lorem ipsum",
+    title: "Ja acabou ?"
+  })
+})();
