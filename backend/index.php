7e1179f6ba7081bd99bb1e8ae20fc51361e5ff5b
Renato Massaro <renato@hackerexperience.com>
2016-05-06 12:27:35 -0300

    Move ajax to index.php

--- backend/index.php
+++ backend/index.php
@@ -1,23 +1,91 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>HE2</title>
-    <meta name="google-signin-scope" content="profile email">
-    <meta name="google-signin-client_id" content="190180432457-3bbiv7l4k0uvovgb1pvvgoul7qrm3839.apps.googleusercontent.com">
-    <script src="https://apis.google.com/js/platform.js" async defer></script>
-</head>
-<body>
 <?php
-session_start();
 
-if (isset($_SESSION['user_id'])) {
-    echo '<p>Welcome back, ' . $_SESSION['user_name'] . '! </p>';
-    echo "<a href='/logout.php'>Log out</a>";
-} else {
-    include('google.example.html');
-    include('login_form.html');
-    include('register_form.html');
+require_once 'classes/Session.php';
+require_once 'classes/User.php';
+require_once 'classes/EmailVerification.php';
+require_once 'classes/Project.php';
+require_once 'classes/Notification.php';
+require_once 'classes/Settings.php';
+require_once 'classes/Email.php';
+
+$result = Array(
+    'status' => FALSE,
+    'msg' => ''
+);
+
+function update_result($status_array) {
+    list($result['status'], $result['msg']) = $status_array;
+    return $result;
 }
-?>
-</body>
-</html>
+
+function function_dispatcher($function) {
+    
+    switch ($function) {
+        
+        case 'validate-user':
+            return update_result(validate_user($_POST['data']));
+            
+        case 'validate-email':
+            return update_result(validate_email($_POST['data']));
+        
+        case 'register-user':
+            return 
+                update_result(
+                    register_user(
+                        $_POST['username'], 
+                        $_POST['password'], 
+                        $_POST['email']
+                    )
+                );
+            
+        case 'verify-email':
+            return update_result(verify_email($_POST['code']));
+            
+        case 'register-teams':
+            return update_result(register_teams($_POST['data']));
+            
+        case 'subscribe-task':
+        case 'unsubscribe-task':
+            return 
+                update_result(
+                    toggle_subscription(
+                        $_POST['task_id'], 
+                        $_POST['team_id'], 
+                        $function
+                    )
+                );
+
+        case 'subscribe-notification':
+        case 'unsubscribe-notification':
+            return 
+                update_result(
+                toggle_notification(
+                        $_POST['notification_desc'], 
+                        $function
+                    )
+                );
+            
+        case 'update-settings':
+            return 
+                update_result(
+                    upsert_setting(
+                        $_POST['setting_name'], 
+                        $_POST['setting_value']
+                    )
+                );
+            
+            
+        default:
+            return update_result(Array(FALSE, 'INVALID_FUNCTION'));
+        
+    }
+    
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_POST['func'])) {
+    $result = function_dispatcher($_POST['func']);
+}
+
+# Return the content in JSON format
+header('Content-type: application/json');
+die(json_encode($result));
