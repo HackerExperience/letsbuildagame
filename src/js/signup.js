251437a4603dc3be2fa132870d2bfe27ce92ca18
Renato Massaro <renato@hackerexperience.com>
2016-05-06 11:01:08 -0300

    Fix ajax url

--- src/js/signup.js
+++ src/js/signup.js
@@ -1,7 +1,7 @@
 function ajax_call(input_data, callback){
     $.ajax({
         type: "POST",
-        url: "http://localhost/lbag/ajax.php",
+        url: "https://ajax.letsbuildagame.org",
         data: input_data,
         success: function(data){
             callback(data);
